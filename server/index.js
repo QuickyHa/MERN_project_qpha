c7cecbecebedc0defcb505023be9c418e90882c4
QuickyHa <qpha@ucsd.edu>
2021-05-12 05:19:18 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(express.json({limit: "30mb", extended: true}));
+app.use(express.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL ='mongodb+srv://quickyha:quickyha619@cluster0.jlb7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedToplogy:true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false)
