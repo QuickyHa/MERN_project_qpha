93d4102dd7b176e7214e797fd47dad5aa238cf89
QuickyHa <qpha@ucsd.edu>
2021-05-13 07:51:47 -0700

    second commit

--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.use('/posts', postRoutes);
 const CONNECTION_URL ='mongodb+srv://quickyha:quickyha619@cluster0.jlb7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedToplogy:true })
