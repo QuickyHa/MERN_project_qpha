ab7e5e898f05696748e1c0e4c30c55ac037a2151
QuickyHa <qpha@ucsd.edu>
2021-05-18 06:48:42 -0700

    deployment

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({limit: "30mb", extended: true}));
 app.use(express.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-const CONNECTION_URL ='mongodb+srv://quickyha:quickyha619@cluster0.jlb7x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+app.get('/', (req,res) => {
+  res.send('HELLO');
+});
+
 const PORT = process.env.PORT || 5000;
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedToplogy:true })
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedToplogy:true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
