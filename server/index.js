7055cbd50fd4f8005f43053ba2d1d27713e42b0f
QuickyHa <qpha@ucsd.edu>
2021-07-15 05:23:12 -0700

    deployment changes

--- server/index.js
+++ server/index.js
@@ -15,7 +15,13 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
 
-const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send('Hello');
+});
+
+const PORT = process.env.PORT;
+
+
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
